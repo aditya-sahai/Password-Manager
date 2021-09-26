684094631d03f85739b7a999fcf7e8c9ec0ed8cb
Buddy <adityasahai925@gmail.com>
2021-09-26 14:49:50 +0530

    Polishing Code

--- main.py
+++ main.py
@@ -4,92 +4,129 @@ from InputMethods import InputMethods
 
 import getpass
 
+class Interface:
+    def __init__(self):
+        """
+        Has input methods and uses PasswordManager and InputMethods classes.
+        """
+        self.Manager = UserManager()
+        self.InputManager = InputMethods()
+        self.user_signed_in = False
 
-Manager = UserManager()
-InputManager = InputMethods()
+    def signin_signup(self):
+        """
+        Asks user if the user wants to sign in or signup and returns true if user logs in succefully.
+        """
+        print("\nEnter 'q' when asked for option number to quit.")
+        print("\n1. Sign In")
+        print("2. Sign Up")
+        signup_or_signin = input("\nEnter Option Number\n>>> ").strip()
 
-print("\nEnter 'q' when asked for option number to quit.")
-print("\n1. Sign In")
-print("2. Sign Up")
-signup_or_signin = input("\nEnter Option Number\n>>> ").strip()
-user_signed_in = False
+        if signup_or_signin == "1":
+            user_input_data = self.InputManager.username_pwd_input()
+            username = user_input_data["username"]
+            password = user_input_data["password"]
+            credentials_validity_user_info = self.Manager.check_user_credentials(username, password)
 
-if signup_or_signin == "1":
-    user_input_data = InputManager.username_pwd_input()
-    username = user_input_data["username"]
-    password = user_input_data["password"]
-    credentials_validity_user_info = Manager.check_user_credentials(username, password)
+            if credentials_validity_user_info["username-match"] and credentials_validity_user_info["password-match"]:
+                user = self.Manager.users[credentials_validity_user_info["user-index"]]
+                print(f"\nSuccesfully logged in as {username}!")
+                self.user_signed_in = True
+                self.username = username
+                self.password = password
 
-    if credentials_validity_user_info["username-match"] and credentials_validity_user_info["password-match"]:
-        user = Manager.users[credentials_validity_user_info["user-index"]]
-        print(f"\nSuccesfully logged in as {username}!")
-        user_signed_in = True
+            else:
+                print("\nInvalid Username/Password.")
 
-    else:
-        print("\nInvalid Username/Password.")
 
-elif signup_or_signin == "2":
-    while True:
-        username = input("\nUsername\n>>> ").strip().lower()
+        elif signup_or_signin == "2":
+            while True:
+                username = input("\nUsername\n>>> ").strip().lower()
 
-        if not Manager.check_user_credentials(username, None)["username-match"]:
-            break
+                if not self.Manager.check_user_credentials(username, None)["username-match"]:
+                    break
+                else:
+                    print("\nUser with that username already exsits. Please enter a different username.")
+
+            password = self.InputManager.confirm_pwd_input()
+            self.Manager.sign_up(username, password)
+            print(f"\nAccount With username, '{username}' created succesfully!")
+            self.user_signed_in = True
+            self.username = username
+            self.password = password
+
+        elif signup_or_signin == "q":
+            self.user_signed_in = False
+
         else:
-            print("\nUser with that username already exsits. Please enter a different username.")
+            print("\nSorry. Could not understand.")
 
-    password = InputManager.confirm_pwd_input()
-    Manager.sign_up(username, password)
-    print(f"\nAccount With username, '{username}' created succesfully!")
-    user_signed_in = True
+    def password_options(self):
+        """
+        Asks the user what the user wants to do and calls the required methods.
+        """
+        print("\n1. View Saved Password")
+        print("2. Write New Password")
+        print("3. Edit Old Password")
+        print("4. Delete Password")
+        print("5. Change Account Password")
+        print("6. Delete Account")
+        option = input("\nEnter Option Number\n>>> ").strip()
 
-elif signup_or_signin == "q":
-    exit()
+        if option == "1":
+            password_info = self.InputManager.app_password_input(True, False)
+            self.PasswordProcessor.view_password(password_info["app"])
 
-else:
-    print("\nSorry. Could not understand.")
+        elif option == "2":
+            password_info = self.InputManager.app_password_input(True, True)
+            self.PasswordProcessor.write_new_password(password_info["app"], password_info["password"])
 
-if user_signed_in:
-    PasswordProcessor = PasswordManager(username)
+        elif option == "3":
+            password_info = self.InputManager.app_password_input(True, True)
+            self.PasswordProcessor.update_saved_password(password_info["app"], password_info["password"])
 
-    print("\n1. View Saved Password")
-    print("2. Write New Password")
-    print("3. Edit Old Password")
-    print("4. Delete Password")
-    print("5. Change Account Password")
-    print("6. Delete Account")
-    option = input("\nEnter Option Number\n>>> ").strip()
+        elif option == "4":
+            password_info = self.InputManager.app_password_input(True, False)
+            self.PasswordProcessor.delete_password(password_info["app"])
 
-    if option == "1":
-        password_info = InputManager.app_password_input(True, False)
-        PasswordProcessor.view_password(password_info["app"])
+        elif option == "5":
+            new_password = self.InputManager.confirm_pwd_input()
+            self.Manager.change_password(self.username, self.password, new_password)
+            print("\nPassword changed succesfully!")
 
-    elif option == "2":
-        password_info = InputManager.app_password_input(True, True)
-        PasswordProcessor.write_new_password(password_info["app"], password_info["password"])
+        elif option == "6":
+            confirmation = getpass.getpass(prompt="\nPress any key to confirm. Press q to cancel.").lower().strip()
+            if confirmation != "q":
+                self.Manager.delete_account(self.username, self.password)
+                self.PasswordProcessor.delete_all_user_passwords()
+                print("\nAccount deleted succesfully!")
 
-    elif option == "3":
-        password_info = InputManager.app_password_input(True, True)
-        PasswordProcessor.update_saved_password(password_info["app"], password_info["password"])
+            self.user_signed_in = False
+            self.signin_signup()
 
-    elif option == "4":
-        password_info = InputManager.app_password_input(True, False)
-        PasswordProcessor.delete_password(password_info["app"])
+            if self.user_signed_in:
+                self.PasswordProcessor = PasswordManager(self.username)
 
-    elif option == "5":
-        new_password = InputManager.confirm_pwd_input()
-        Manager.change_password(username, password, new_password)
-        print("\nPassword changed succesfully!")
+        elif option == "q":
+            self.user_signed_in = False
 
-    elif option == "6":
-        confirmation = getpass.getpass(prompt="\nPress any key to confirm. Press q to cancel.").lower().strip()
-        if confirmation != "q":
-            Manager.delete_account(username, password)
-            PasswordProcessor.delete_all_user_passwords()
+        else:
+            print("\nSorry. Could not understand.")
 
-        print("\nAccount deleted succesfully!")
+    def main(self):
+        """
+        Has the main loop.
+        """
+        self.signin_signup()
 
-    elif option == "q":
-        exit()
+        if self.user_signed_in:
+            self.PasswordProcessor = PasswordManager(self.username)
 
-    else:
-        print("\nSorry. Could not understand.")
+            while self.user_signed_in:
+                print("\nEnter 'q' when asked for option number to exit loop.")
+                self.password_options()
+
+
+if __name__ == "__main__":
+    UI = Interface()
+    UI.main()
